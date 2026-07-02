73fa12c82423ba4544e5bb3d9348a3791dee0240
Andre Gallo <9999415+afgallo@users.noreply.github.com>
2026-07-02 18:56:42 +1000

    feat(wm): default to 6 workspaces instead of 9

--- test/test_wm_seed.zsh
+++ test/test_wm_seed.zsh
@@ -19,9 +19,13 @@ contains "aerospace starts at login"     "start-at-login = true"        "$aero"
 contains "aerospace binds cmd modifier"  "cmd-1 = 'workspace 1'"        "$aero"
 contains "aerospace closes on cmd-q"     "cmd-q = 'close'"              "$aero"
 contains "aerospace notifies sketchybar" "exec-on-workspace-change"     "$aero"
+# Default is 6 workspaces: the 6th is bound, the 7th is not.
+contains "aerospace binds 6th workspace"  "cmd-6 = 'workspace 6'"        "$aero"
+check "aerospace stops at 6 workspaces" "no" "$([[ "$aero" == *"workspace 7"* ]] && print yes || print no)"
 
 sbrc="$(<"$ROOT/wm/sketchybar/sketchybarrc")"
 contains "sketchybarrc sources colors" "colors.sh" "$sbrc"
+contains "sketchybar loops 6 spaces" "for sid in 1 2 3 4 5 6; do" "$sbrc"
 
 tweaks="$(<"$ROOT/wm/tweaks.conf")"
 contains "tweaks set fast key repeat" "NSGlobalDomain KeyRepeat int 2" "$tweaks"
