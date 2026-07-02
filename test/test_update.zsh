c9eb90e8c83b6955d48c1cc55d98dc8adf8e88b3
Andre Gallo <9999415+afgallo@users.noreply.github.com>
2026-07-02 12:03:46 +1000

    feat(software): drive omac update through the software engine

--- test/test_update.zsh
+++ test/test_update.zsh
@@ -16,8 +16,16 @@ export OMAC_CONFIG="$(mktemp -d)"
 export OMAC_STATE="$(mktemp -d)"
 export OMAC_MIGRATIONS_STATE="$OMAC_STATE/migrations"
 
+source "$ROOT/test/software_stubs.zsh"
+export OMAC_SOFTWARE="$(mktemp -d)/software"
+mkdir -p "$OMAC_SOFTWARE/groups"
+print -r -- 'brew "ripgrep"' > "$OMAC_SOFTWARE/groups/shell.Brewfile"
+print -r -- 'node@lts'        > "$OMAC_SOFTWARE/runtimes.manifest"
+_stub_setup
+
 out="$(zsh "$fake/bin/omac" update 2>&1)"
 check "update exits 0" "0" "$?"
 contains "update reports completion" "update complete" "$out"
 check "update ran migrations" "1" "$(ls "$OMAC_MIGRATIONS_STATE" | grep -c example)"
+contains "update installed software via engine" "shell.Brewfile" "$(<"$BREW_LOG")"
 finish
